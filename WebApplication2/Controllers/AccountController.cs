17b14365f43c8252252cd901252cad6118db5330
Cihan Sungur <sungur1988@gmail.com>
2022-06-14 22:48:46 +0300

    HomeworkCompleted

---
+++ WebApplication2/Controllers/AccountController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebApplication2.Data;
+using WebApplication2.Models;
+
+namespace JwtToken.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly TokenOption tokenOption;
+        public AccountController(UserManager<IdentityUser> userManager, IOptions<TokenOption>options) =>
+            (_userManager, tokenOption) = (userManager, options.Value);
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginDto login)
+        {
+            List<Claim> claims = new List<Claim>();
+            var user = await _userManager.FindByEmailAsync(login.Email);
+            if (user == null) throw new Exception("");
+
+            var result = await _userManager.CheckPasswordAsync(user, login.Password);
+            if (result)
+            {
+
+                var roles = await _userManager.GetRolesAsync(user);
+                foreach (var role in roles)
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, role));
+                }
+
+                claims.Add(new Claim(ClaimTypes.Name, user.Email));
+                claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+
+                var token = GetToken(claims);
+
+                var handler = new JwtSecurityTokenHandler();
+                string jwt = handler.WriteToken(token);
+
+                return Ok(new
+                {
+                    token = jwt,
+                    expiration = token.ValidTo
+                });
+            }
+
+            return Unauthorized();
+        }
+        
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegisterDto request)
+        {
+            if (_userManager.Users.Any(u => u.Email == request.Email))
+            {
+                return BadRequest("User already exists.");
+            };
+
+            var user = new IdentityUser { Email=request.Email ,UserName=request.UserName};
+
+            var result = await _userManager.CreateAsync(user, request.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors.Select(x=>x));
+            }
+
+            return Ok("User Created");
+        }
+
+
+
+        private JwtSecurityToken GetToken(List<Claim> claims)
+        {
+
+            var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenOption.Key));
+
+            var token = new JwtSecurityToken(
+
+                 signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256),
+                 issuer: tokenOption.Issuer,
+                 audience: tokenOption.Audience,
+                 expires: DateTime.Now.AddMinutes(tokenOption.Expiration),
+                 claims: claims
+                );
+
+            return token;
+
+        }
+
+    }
+}
---
+++ WebApplication2/Models/Group.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication2.Models
+{
+    public class Group
+    {
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+        public virtual ICollection<IdentityUser> GroupMembers { get; set; }
+        public IdentityUser GroupOwner { get; set; }
+    }
+}
